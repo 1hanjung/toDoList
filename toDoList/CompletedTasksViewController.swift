4ddc6f5f6845fa801835fce656442338ce79a724
1hanjung <jungillhan@naver.com>
2023-09-01 13:57:16 +0900

    내용수정

---
+++ toDoList/CompletedTasksViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CompletedTasksViewController.swift
+//  toDoList
+//
+//  Created by 정일한 on 2023/08/30.
+//
+
+import UIKit
+
+
+class CompletedTasksViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "완료한일 보기"
+    }
+}
+
